979bb6b64e9a6d9ac9bb1a62aee6c75679fe2fb1
Rouven Lührs <rouven@luehrs.dev>
2021-11-08 12:50:54 +0100

    added new page

---
+++ pages/portfolio/photos.js
@@ -0,0 +1,11 @@
+import { Box, Heading } from "@chakra-ui/layout"
+
+const PhotoPage = () => {
+    return (
+        <Box>
+            <Heading>Photos</Heading>
+        </Box>
+    );
+}
+
+export default PhotoPage;
