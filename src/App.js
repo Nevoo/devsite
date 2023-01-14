10d065e3ff9b7976af612337612c1505d6c93670
Rouven Lührs <rouven@luehrs.dev>
2023-01-14 16:45:08 +0100

    playing around with drei materials

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { Canvas } from '@react-three/fiber';
 import DonutGLTF from './compontents/donut-gltf';
 import { OrbitControls } from '@react-three/drei/core';
-import { PerspectiveCamera } from "@react-three/drei";
+import { PerspectiveCamera, GradientTexture, MeshReflectorMaterial, MeshWobbleMaterial } from "@react-three/drei";
 
 function App() {
   return (
@@ -16,13 +16,46 @@ function App() {
           near={0.1}
           fov={22.9}
           position={[0, 0, 0.5]}
-          rotation={[0, Math.PI / 2, 0]}
+          rotation={[0, 0, 0]}
         />
         <ambientLight intensity={0.3}/>
-        <spotLight  position={[10, 10, 10]} angle={0.30} penumbar={1}/>
-        <pointLight position={[-10,-10,-10]}/>
+        <spotLight  position={[10, 10, 10]} angle={0.30} />
+        <pointLight position={[-10,-10, -10]}/>
         <Suspense fallback={null}>
           <DonutGLTF />
+          <mesh rotation={[-Math.PI / 2, 0, Math.PI / 2]} position={[0, -0.07, 0]}>
+            <planeGeometry />
+            <MeshReflectorMaterial
+              blur={[300, 100]}
+              resolution={2048}
+              mixBlur={1}
+              mixStrength={50}
+              roughness={1}
+              depthScale={1.2}
+              minDepthThreshold={0.4}
+              maxDepthThreshold={1.4}
+              color = "#050505"
+              metalness={0.5}
+            > 
+                    <GradientTexture stops={[0, 0.46, 1]} colors={['#12c2e9', '#c471ed', '#f64f59']} size={100} />
+
+                        {/* <MeshWobbleMaterial factor={0.1} speed={1} /> */}
+
+            </MeshReflectorMaterial>
+          </mesh>
+          {/* <Plane receiveShadow={true} position={[0, -0.07, 0]} rotation={[Math.PI * -0.5, 0, 0]}/>
+          <MeshReflectorMaterial
+            blur={[300, 100]}
+            resolution={2048}
+            mixBlur={1}
+            mixStrength={50}
+            roughness={1}
+            depthScale={1.2}
+            minDepthThreshold={0.4}
+            maxDepthThreshold={1.4}
+            color="#050505"
+            metalness={0.5}
+          /> */}
           <OrbitControls />
         </Suspense>
       </Canvas>
