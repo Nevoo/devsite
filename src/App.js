6a7bcbd0eb0873db9c05c24a89fe80278ef727a3
Rouven Lührs <rouven@luehrs.dev>
2023-01-14 15:54:57 +0100

    added a new donut model with sprinkles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, {Suspense} from 'react';
 import './App.css';
 import { Canvas } from '@react-three/fiber';
-import Donut from './compontents/donut';
+import DonutGLTF from './compontents/donut-gltf';
 import { OrbitControls } from '@react-three/drei/core';
+import { PerspectiveCamera } from "@react-three/drei";
 
 function App() {
   return (
     <div className="App">
       <Canvas>
+        <PerspectiveCamera
+          name="Camera"
+          makeDefault={true}
+          far={100}
+          near={0.1}
+          fov={22.9}
+          position={[0, 0, 0.5]}
+          rotation={[0, Math.PI / 2, 0]}
+        />
         <ambientLight intensity={0.3}/>
-        <spotLight  position={[10,10,10]} angle={0.15} penumbar={1}/>
+        <spotLight  position={[10, 10, 10]} angle={0.30} penumbar={1}/>
         <pointLight position={[-10,-10,-10]}/>
         <Suspense fallback={null}>
-          <Donut />
+          <DonutGLTF />
           <OrbitControls />
         </Suspense>
       </Canvas>
