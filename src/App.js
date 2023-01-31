f09acec13e291564a8a29e6c665ede0a6d3fdc8e
Rouven Lührs <rouven@luehrs.dev>
2023-01-31 12:34:36 +0100

    added floating bubbles and a glassy torus based on a spline example

--- src/App.js
+++ src/App.js
@@ -8,10 +8,20 @@ import {
   Instance,
   Environment,
   OrbitControls,
+  Float,
+  MeshTransmissionMaterial,
+  Lightformer,
 } from "@react-three/drei";
 import { useSpring, animated, config } from "@react-spring/web";
-import { EffectComposer, SSAO } from "@react-three/postprocessing";
+import {
+  EffectComposer,
+  SSAO,
+  Bloom,
+  DepthOfField,
+} from "@react-three/postprocessing";
 import { MathUtils } from "three";
+import useSpline from "@splinetool/r3f-spline";
+import { useControls } from "leva";
 
 const particles = Array.from({ length: 150 }, () => ({
   factor: MathUtils.randInt(2, 10),
@@ -51,49 +61,159 @@ function App() {
           <OrbitControls></OrbitControls>
           <PerspectiveCamera
             name="Camera"
-            makeDefault={true}
+            makeDefault
             far={100}
             near={0.1}
             fov={22.9}
             position={[0, 0, 1]}
             rotation={[0, 0, 0]}
           />
-          <ambientLight intensity={0.3} />
-          <spotLight position={[10, 10, 10]} angle={0.3} />
-          <pointLight position={[-10, -10, -10]} />
+          <color attach="background" args={["#fef4ef"]} />
+          <ambientLight />
+          <directionalLight castShadow intensity={0.6} position={[0, 0, 10]} />
           <EffectComposer multisampling={4}>
-            <SSAO
+            {/* <SSAO
               samples={40}
               radius={0.1}
               intensity={30}
               luminanceInfluence={0.1}
               color="red"
+            /> */}
+            {/* <Bloom
+              luminanceThreshold={0}
+              mipmapBlur
+              luminanceSmoothing={0}
+              intensity={0.2}
             />
+            <DepthOfField
+              target={[0, 0, 3]}
+              focalLength={1.8}
+              bokehScale={15}
+              height={700}
+            /> */}
           </EffectComposer>
-          <Instances
-            limit={particles.length} // Optional: max amount of items (for calculating buffer size)
-            castShadow
-            receiveShadow
-            // range={150}
-            position={[0, 0, 0]}
-          >
-            <sphereGeometry args={[0.01, 32, 32]} />
-            <meshStandardMaterial roughness={0} color="#f0f0f0" />
-            {particles.map((data, i) => (
-              <Bubble key={i} {...data} />
-            ))}
-          </Instances>
+          {/* <Bubbles></Bubbles> */}
           <Suspense fallback={null}>
             <DonutGLTF onHover={onHover} />
-            <Environment preset="city" />
+            <Environment resolution={256}>
+              <group rotation={[-Math.PI / 2, 0, 0]}>
+                <Lightformer
+                  intensity={4}
+                  rotation-x={Math.PI / 2}
+                  position={[0, 5, -9]}
+                  scale={[10, 10, 1]}
+                />
+                {[2, 0, 2, 0, 2, 0, 2, 0].map((x, i) => (
+                  <Lightformer
+                    key={i}
+                    form="circle"
+                    intensity={4}
+                    rotation={[Math.PI / 2, 0, 0]}
+                    position={[x, 4, i * 4]}
+                    scale={[4, 1, 1]}
+                  />
+                ))}
+                <Lightformer
+                  intensity={2}
+                  rotation-y={Math.PI / 2}
+                  position={[-5, 1, -1]}
+                  scale={[50, 2, 1]}
+                />
+                <Lightformer
+                  intensity={2}
+                  rotation-y={Math.PI / 2}
+                  position={[-5, -1, -1]}
+                  scale={[50, 2, 1]}
+                />
+                <Lightformer
+                  intensity={2}
+                  rotation-y={-Math.PI / 2}
+                  position={[10, 1, 0]}
+                  scale={[50, 2, 1]}
+                />
+              </group>
+            </Environment>
           </Suspense>
+          <Bubbles color="#29C1A2" config={config}></Bubbles>
+          <Scene scale={0.0005}></Scene>
         </Canvas>
       </animated.div>
     </div>
   );
 }
 
-function Bubble({ factor, speed, xFactor, yFactor, zFactor }) {
+function Scene({ ...props }) {
+  const config = useControls({
+    backside: false,
+    samples: { value: 16, min: 1, max: 32, step: 1 },
+    resolution: { value: 256, min: 64, max: 2048, step: 64 },
+    transmission: { value: 0.95, min: 0, max: 1 },
+    roughness: { value: 0.5, min: 0, max: 1, step: 0.01 },
+    clearcoat: { value: 0.1, min: 0, max: 1, step: 0.01 },
+    clearcoatRoughness: { value: 0.1, min: 0, max: 1, step: 0.01 },
+    thickness: { value: 200, min: 0, max: 200, step: 0.01 },
+    backsideThickness: { value: 200, min: 0, max: 200, step: 0.01 },
+    ior: { value: 1.5, min: 1, max: 5, step: 0.01 },
+    chromaticAberration: { value: 1, min: 0, max: 1 },
+    anisotropy: { value: 1, min: 0, max: 10, step: 0.01 },
+    distortion: { value: 0, min: 0, max: 1, step: 0.01 },
+    distortionScale: { value: 0.2, min: 0.01, max: 1, step: 0.01 },
+    temporalDistortion: { value: 0, min: 0, max: 1, step: 0.01 },
+    attenuationDistance: { value: 0.5, min: 0, max: 10, step: 0.01 },
+    attenuationColor: "#ffffff",
+    color: "#ffffff",
+  });
+
+  return (
+    <>
+      <group {...props}>
+        <Shape name="Torus" float={0} color="#fef4ef" config={config} />
+      </group>
+    </>
+  );
+}
+
+function Shape({ name, float = 300, color, config, ...props }) {
+  const { nodes } = useSpline("/shapes.splinecode");
+  return (
+    <Float floatIntensity={float} rotationIntensity={0} speed={2}>
+      <mesh renderOrder={100} geometry={nodes[name].geometry} {...props}>
+        <MeshTransmissionMaterial
+          {...config}
+          color={color}
+          toneMapped={false}
+        />
+      </mesh>
+    </Float>
+  );
+}
+
+const Bubbles = ({ color, config, ...props }) => {
+  return (
+    <Instances
+      limit={particles.length} // Optional: max amount of items (for calculating buffer size)
+      castShadow
+      receiveShadow
+      // range={150}
+      position={[0, 0, 0]}
+    >
+      <sphereGeometry args={[0.01, 32, 32]} />
+      <MeshTransmissionMaterial {...config} color={color} toneMapped={false} />
+      {particles.map((data, i) => {
+        var bubbleColor = "#FF9060";
+        if (i % 3 === 0) {
+          bubbleColor = "#fef4ef";
+        } else if (i % 5 === 0) {
+          bubbleColor = color;
+        }
+        console.log(bubbleColor);
+        return <Bubble color={bubbleColor} key={i} {...data} />;
+      })}
+    </Instances>
+  );
+};
+
+function Bubble({ color, factor, speed, xFactor, yFactor, zFactor }) {
   const ref = useRef();
   useFrame((state) => {
     const t = factor + state.clock.elapsedTime * (speed / 2);
@@ -117,7 +237,7 @@ function Bubble({ factor, speed, xFactor, yFactor, zFactor }) {
         (Math.sin(t * 3) * factor) / positionFactor
     );
   });
-  return <Instance ref={ref} />;
+  return <Instance color={color} ref={ref} />;
 }
 
 export default App;
