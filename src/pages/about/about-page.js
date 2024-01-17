5e4f7670cd9ed92269b4f372e6f2eecf8f2a0d27
Rouven Lührs <rouven@luehrs.dev>
2024-01-17 14:52:12 +0100

    adjusted images and logic for scrolling on about page

--- src/pages/about/about-page.js
+++ src/pages/about/about-page.js
@@ -3,7 +3,7 @@ import { useThree } from "@react-three/fiber";
 import { animated, config, useSpring } from "@react-spring/web";
 
 export const AboutPage = () => {
-    const { width } = useThree((state) => state.size);
+    const { width, height } = useThree((state) => state.size);
 
     const { opacity } = useSpring({
         config: config.stiff,
@@ -12,84 +12,81 @@ export const AboutPage = () => {
     });
 
     return (
-        <group>
-            <MobileScrollWrapper width={width}>
-                <Html fullscreen position={[0, -2.2, 0]}>
-                    <animated.div
-                        className="body"
+        <MobileScrollWrapper width={width} height={height}>
+            <Html fullscreen position={[0, -2.2, 0]}>
+                <animated.div
+                    className="body"
+                    style={{
+                        display: "flex",
+                        opacity,
+                        flexDirection: width > 1000 ? "row" : "column",
+                        justifyContent: "space-between",
+                        textAlign: "left",
+                    }}
+                >
+                    <div
                         style={{
                             display: "flex",
-                            opacity,
-                            flexDirection: width > 1000 ? "row" : "column",
-                            justifyContent: "space-between",
-                            textAlign: "left",
+                            flexDirection: "column",
+                            justifyContent: "center",
+                            width: width > 1000 ? "50%" : "100%",
                         }}
                     >
-                        <div
+                        <h1
                             style={{
-                                display: "flex",
-                                flexDirection: "column",
-                                justifyContent: "center",
-                                width: width > 1000 ? "50%" : "100%",
+                                fontFamily: "Gilroy",
+                                fontSize: width > 1000 ? "7em" : "4em",
+                                fontWeight: "bold",
                             }}
                         >
-                            <h1
-                                style={{
-                                    fontFamily: "Gilroy",
-                                    fontSize: width > 1000 ? "7em" : "4em",
-                                    fontWeight: "bold",
-                                }}
-                            >
-                                ABOUT ME
-                            </h1>
+                            ABOUT ME
+                        </h1>
 
-                            <div
-                                style={{
-                                    fontFamily: "Gilroy",
-                                    fontSize: "1.8em",
-                                    fontWeight: "bold",
-                                }}
-                            >
-                                <p>
-                                    I'm a creative developer working on apps and
-                                    web projects all the time. I started this
-                                    journey when I was 16 years old and have
-                                    been working full time as a developer since
-                                    2018.
-                                </p>
-                                <p>
-                                    Besides that, for almost the same amount of
-                                    time, I've been a photographer. Starting
-                                    with just my phone in 2019 I worked my way
-                                    up to a professional camera and now even
-                                    doing paid work for clients sometimes.
-                                </p>
-                            </div>
-                        </div>
                         <div
                             style={{
-                                paddingTop: "5em",
-                                width: "40%",
-                                height: "40%",
+                                fontFamily: "Gilroy",
+                                fontSize: "1.8em",
+                                fontWeight: "bold",
                             }}
                         >
-                            <img
-                                src="KPK07115.jpg"
-                                style={{
-                                    maxWidth: "100%",
-                                    maxHeight: "100%",
-                                }}
-                            />
+                            <p>
+                                I'm a creative developer working on apps and web
+                                projects all the time. I started this journey
+                                when I was 16 years old and have been working
+                                full time as a developer since 2018.
+                            </p>
+                            <p>
+                                Besides that, for almost the same amount of
+                                time, I've been a photographer. Starting with
+                                just my phone in 2019 I worked my way up to a
+                                professional camera and now even doing paid work
+                                for clients sometimes.
+                            </p>
                         </div>
-                    </animated.div>
-                </Html>
-            </MobileScrollWrapper>
-        </group>
+                    </div>
+                    <div
+                        style={{
+                            paddingTop: "5em",
+                            width: "40%",
+                            height: "40%",
+                        }}
+                    >
+                        <img
+                            src="KPK07115.jpg"
+                            style={{
+                                maxWidth: "100%",
+                                maxHeight: "100%",
+                            }}
+                        />
+                    </div>
+                </animated.div>
+            </Html>
+        </MobileScrollWrapper>
     );
 };
 
-const MobileScrollWrapper = ({ children, width }) => {
-    return width > 1000 ? (
+const MobileScrollWrapper = ({ children, width, height }) => {
+    return width > 1000 && height > 1000 ? (
         <>{children}</>
     ) : (
         <ScrollControls pages="2">
