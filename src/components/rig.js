16dbe939e179c1cad08b339ed0738ebea8ad450f
Rouven Lührs <rouven@luehrs.dev>
2024-01-23 17:04:30 +0100

    adjustments to make rig.js more readable

--- src/components/rig.js
+++ src/components/rig.js
@@ -1,18 +1,23 @@
 import { useFrame } from "@react-three/fiber";
 import { easing } from "maath";
 
-export const Rig = () => {
+export const Rig = ({ isStatic }) => {
     useFrame((state, delta) => {
-        easing.damp3(
-            state.camera.position,
-            [
-                Math.sin(-state.pointer.x) * 5,
-                state.pointer.y * 3.5,
-                15 + Math.cos(state.pointer.x) * 10,
-            ],
-            0.2,
-            delta
-        );
+        // const cameraPos = isStatic
+        //     ? [0, 0, 0]
+        //     : [
+        //           Math.sin(-state.pointer.x) * 5,
+        //           state.pointer.y * 3.5,
+        //           15 + Math.cos(state.pointer.x) * 10,
+        //       ];
+
+        const cameraPos = [
+            Math.sin(-state.pointer.x) * 5,
+            state.pointer.y * 3.5,
+            15 + Math.cos(state.pointer.x) * 10,
+        ];
+
+        easing.damp3(state.camera.position, cameraPos, 0.2, delta);
         state.camera.lookAt(0, 0, 0);
     });
 };
