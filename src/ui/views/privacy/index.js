041b332eddad840c65f28ecdf9cfa2bdfe9cbad1
Rouven Lührs <rouven@luehrs.dev>
2024-03-12 15:35:22 +0100

    adjusted privacy texts

--- src/ui/views/privacy/index.js
+++ src/ui/views/privacy/index.js
@@ -29,7 +29,7 @@ export const Privacy = () => {
 
 Effective Date: 01.03.2024
 
-Thank you for visiting rouvens work (the "Website"), owned and operated by Rouven Lührs. 
+Thank you for visiting rouvens work (the "Website").
 Your privacy is important to me, and I am committed to protecting your personal information. 
 This Privacy Policy outlines the types of information collected when you visit our Website, 
 how I use and protect that information, and your rights regarding your personal data.
@@ -38,38 +38,30 @@ As the owner and operator of rouvens work, I prioritize privacy and data complia
 I utilize Vercel Web Analytics to gather insights about website traffic while ensuring user privacy.
 
 Data collected:
-Vercel Web Analytics operates globally and aligns with leading data protection standards.
-It anonymizes data points such as pageviews and custom events, ensuring no personal identifiers are collected. 
-The recorded data points, including:
-
-    - Event Timestamp: 2020-10-29 09:06:30
-    - URL: /blog/nextjs-10
-    - Dynamic Path: /blog/[slug]
-    - Referrer: https://twitter.com/
-    - Query Params (Filtered): ?ref=hackernews
-    - Geolocation: US, California, San Francisco
-    - Device OS & Version: Android 10
-    - Browser & Version: Chrome 86 (Blink)
-    - Device Type: Mobile (or Desktop/Tablet)
-    - Web Analytics Script Version: 1.0.0
-
-Visitor identification and data storage:
-Vercel Web Analytics identifies visitors using a hash created from the incoming request, ensuring privacy without third-party cookies. 
-Visitor session data is automatically discarded after 24 hours. 
-All page views, including fresh loads and client-side transitions, are tracked without storing personally identifiable information.
-
-Purpose of Data Collection:
-The non-personal data I collect is used for web analytics purposes only. 
-I use this information to track user activity and improve the functionality and performance of our Website.
-
-Data Sharing:
-I do not share any data collected from our Website with third parties.
-All data collected is used solely for internal analytics purposes.
-
-Children's Privacy:
-I do not knowingly collect any personal information from children under the age of 13.
-Our Website is not intended for use by children, and I do not target or market to individuals under the age of 13.
-
+Vercel Analytics: To enhance our website, we use Vercel Web Analytics. 
+These tools collect aggregated data to provide insights into website usage and performance, with no individual visitor identification. 
+For more information on Vercel's data practices, please refer to their privacy policies:
+    `}
+                    </pre>
+                    <div
+                        style={{
+                            fontFamily: "Inter",
+                            textAlign: "left",
+                        }}
+                    >
+                        <a href="https://vercel.com/docs/analytics/privacy-policy">
+                            Vercel Web Analytics Privacy and Compliance
+                        </a>
+                    </div>
+                    <pre
+                        style={{
+                            fontFamily: "Inter",
+                            textAlign: "left",
+                            whiteSpace: "pre-wrap",
+                            wordWrap: "break-word",
+                        }}
+                    >
+                        {`
 Updates to the Privacy Policy:
 This Privacy Policy may be updated periodically to reflect changes in our privacy practices.
 I will notify users of any material changes by posting the updated Privacy Policy on our Website.
