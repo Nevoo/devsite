9c599458a2b8056bd373b9f1f3576777cebfc82f
Rouven Lührs <rouven@luehrs.dev>
2024-05-29 15:54:56 +0200

    all images vertical

--- src/ui/views/landing-view/state/image-state.js
+++ src/ui/views/landing-view/state/image-state.js
@@ -8,6 +8,9 @@ const gallery = (id, category) =>
     `${galleryBase}/${category}/gallery/DSC${id}.jpeg`;
 const cover = (category) => `${galleryBase}/${category}/cover.jpeg`;
 
+const horizontalScale = [3, 2, 0];
+const verticalScale = [2, 3, 0];
+
 // TODO: all this should be handled through a cms, s3 bucket or something
 const useImageState = create((set) => ({
     categories: [
@@ -22,45 +25,45 @@ const useImageState = create((set) => ({
     // index is the id of the category
     images: [
         [
-            { url: gallery("00880", "animals"), scale: [3, 2, 0] },
-            { url: gallery("8270", "animals"), scale: [2, 3, 0] },
+            { url: gallery("00880", "animals"), scale: verticalScale },
+            { url: gallery("8270", "animals"), scale: verticalScale },
         ], // animals
         [
-            { url: gallery("03694", "nature"), scale: [2, 3, 0] },
-            { url: gallery("03828", "nature"), scale: [2, 3, 0] },
-            { url: gallery("03830", "nature"), scale: [3, 2, 0] },
-            { url: gallery("03855", "nature"), scale: [3, 2, 0] },
-            { url: gallery("03588", "nature"), scale: [2, 3, 0] },
-            { url: gallery("8162", "nature"), scale: [2, 3, 0] },
+            { url: gallery("03694", "nature"), scale: verticalScale },
+            { url: gallery("03828", "nature"), scale: verticalScale },
+            { url: gallery("03830", "nature"), scale: verticalScale },
+            { url: gallery("03855", "nature"), scale: verticalScale },
+            { url: gallery("03588", "nature"), scale: verticalScale },
+            { url: gallery("8162", "nature"), scale: verticalScale },
         ], // nature
         [
-            { url: gallery("03743", "travel"), scale: [2, 3, 0] },
-            { url: gallery("03700", "travel"), scale: [2, 3, 0] },
-            { url: gallery("03862", "travel"), scale: [2, 3, 0] },
-            { url: gallery("03816", "travel"), scale: [2, 3, 0] },
-            { url: gallery("8251", "travel"), scale: [3, 2, 0] },
+            { url: gallery("03743", "travel"), scale: verticalScale },
+            { url: gallery("03700", "travel"), scale: verticalScale },
+            { url: gallery("03862", "travel"), scale: verticalScale },
+            { url: gallery("03816", "travel"), scale: verticalScale },
+            { url: gallery("8251", "travel"), scale: verticalScale },
         ], // travel
         [
-            { url: gallery("05320", "street"), scale: [2, 3, 0] },
-            { url: gallery("05299", "street"), scale: [2, 3, 0] },
-            { url: gallery("03647", "street"), scale: [2, 3, 0] },
+            { url: gallery("05320", "street"), scale: verticalScale },
+            { url: gallery("05299", "street"), scale: verticalScale },
+            { url: gallery("03647", "street"), scale: verticalScale },
         ], // street
         [
-            { url: gallery("04137", "concerts"), scale: [3, 2, 0] },
-            { url: gallery("04159", "concerts"), scale: [2, 3, 0] },
-            { url: gallery("04230", "concerts"), scale: [2, 3, 0] },
-            { url: gallery("04248", "concerts"), scale: [3, 2, 0] },
-            { url: gallery("04330", "concerts"), scale: [3, 2, 0] },
-            { url: gallery("04360", "concerts"), scale: [2, 3, 0] },
+            { url: gallery("04137", "concerts"), scale: verticalScale },
+            { url: gallery("04159", "concerts"), scale: verticalScale },
+            { url: gallery("04230", "concerts"), scale: verticalScale },
+            { url: gallery("04248", "concerts"), scale: verticalScale },
+            { url: gallery("04330", "concerts"), scale: verticalScale },
+            { url: gallery("04360", "concerts"), scale: verticalScale },
         ], // concerts
         // [{ url: gallery("09296", "events"), scale: [3, 2, 0] }], // events
         [
-            { url: gallery("02799", "weddings"), scale: [3, 2, 0] },
-            { url: gallery("02640", "weddings"), scale: [3, 2, 0] },
-            { url: gallery("02729", "weddings"), scale: [2, 3, 0] },
-            { url: gallery("02936", "weddings"), scale: [2, 3, 0] },
-            { url: gallery("02847", "weddings"), scale: [2, 3, 0] },
-            { url: gallery("02597", "weddings"), scale: [3, 2, 0] },
+            { url: gallery("02799", "weddings"), scale: verticalScale },
+            { url: gallery("02640", "weddings"), scale: verticalScale },
+            { url: gallery("02729", "weddings"), scale: verticalScale },
+            { url: gallery("02936", "weddings"), scale: verticalScale },
+            { url: gallery("02847", "weddings"), scale: verticalScale },
+            { url: gallery("02597", "weddings"), scale: verticalScale },
         ], // weddings
     ],
 
