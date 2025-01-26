21a707abdddc05dddbef7716e429752cbc3831ea
Rouven Lührs <rouven@luehrs.dev>
2025-01-26 22:49:06 +0100

    commented unused landing page component

--- src/app/page.js
+++ src/app/page.js
@@ -5,17 +5,17 @@ import dynamic from "next/dynamic";
 import LoadingScreen from "../components/LoadingScreen";
 
 // Dynamically import LandingPage with delay
-const LandingPage = dynamic(
-  () =>
-    new Promise((resolve) => {
-      setTimeout(() => {
-        import("../components/LandingPage").then(resolve);
-      }, 2000);
-    }),
-  {
-    ssr: false,
-  }
-);
+// const LandingPage = dynamic(
+//   () =>
+//     new Promise((resolve) => {
+//       setTimeout(() => {
+//         import("../components/LandingPage").then(resolve);
+//       }, 2000);
+//     }),
+//   {
+//     ssr: false,
+//   }
+// );
 
 const Scene = dynamic(() => import("../components/scene/Scene"), {
   ssr: false,
