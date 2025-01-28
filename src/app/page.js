800db8e3162dbd2d92e084f14ba3e5d43d595f0d
Rouven Lührs <rouven@luehrs.dev>
2025-01-28 17:22:39 +0100

    implementing trash scroll snapping

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,13 @@ import { Suspense } from "react";
 import dynamic from "next/dynamic";
 import LoadingScreen from "../components/LoadingScreen";
 
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { Observer } from "gsap/Observer";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(Observer, ScrollTrigger, useGSAP);
+
 // Dynamically import LandingPage with delay
 // const LandingPage = dynamic(
 //   () =>
@@ -23,7 +30,7 @@ const Scene = dynamic(() => import("../components/scene/Scene"), {
 
 export default function Home() {
   return (
-    <main>
+    <main className="relative">
       <Suspense fallback={null}>
         <Scene />
       </Suspense>
