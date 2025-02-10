4df3d079906884c7160667bac60f84421049c3cb
Rouven Lührs <rouven@luehrs.dev>
2025-02-10 22:45:35 +0200

    fixed navbar blend mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,35 +5,20 @@ import "./globals.css";
 import NavigationMenu from "../components/NavigationMenu";
 import { useExploreState } from "../state/explore";
 import Link from "next/link";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion } from "framer-motion";
 
 export default function RootLayout({ children }) {
   const setIsExploring = useExploreState((state) => state.setIsExploring);
-  const { scrollYProgress } = useScroll();
-
-  // Create a color transform based on scroll position
-  const textColor = useTransform(
-    scrollYProgress,
-    [0.4, 0.5], // adjust these values to match when the white section appears
-    ["#FFFFFF", "#000000"]
-  );
 
   return (
     <html lang="en">
       <body>
-        <div className="absolute z-10">
-          <header className="fixed top-0 left-0 right-0 p-6">
-            <Link href="/" onClick={() => setIsExploring(false)}>
-              <motion.h1 
-                className="text-2xl font-bold"
-                style={{ color: textColor }}
-              >
-                rouven
-              </motion.h1>
-            </Link>
-            <NavigationMenu />
-          </header>
-        </div>
+        <header className="header-nav p-6">
+          <Link href="/" onClick={() => setIsExploring(false)}>
+            <motion.h1 className="text-2xl font-bold">rouven</motion.h1>
+          </Link>
+          <NavigationMenu />
+        </header>
         {children}
       </body>
     </html>
